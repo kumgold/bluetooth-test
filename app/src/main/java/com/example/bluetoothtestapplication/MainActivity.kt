f9e4288cb017471626c49253322a3629c0fb891b
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-07-05 22:49:15 +0900

    bluetooth scan

--- app/src/main/java/com/example/bluetoothtestapplication/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothtestapplication/MainActivity.kt
@@ -1,20 +1,65 @@
 package com.example.bluetoothtestapplication
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.bluetoothtestapplication.ui.theme.BluetoothTestApplicationTheme
 
+private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
+
+private val BluetoothAdapter.isDisabled: Boolean
+    get() = !isEnabled
+
 class MainActivity : ComponentActivity() {
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // TODO
+        }
+    }
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            super.onScanResult(callbackType, result)
+
+
+        }
+    }
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private var isScanning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkBluetoothFeature()
+        launchBluetoothAdapterIntent()
+
+        scanBluetoothLEDevices(bluetoothAdapter?.isEnabled ?: false)
+
         setContent {
             BluetoothTestApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +67,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun checkBluetoothFeature() {
+        packageManager.takeIf { it.missingSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE) }?.also {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BluetoothTestApplicationTheme {
-        Greeting("Android")
+    private fun launchBluetoothAdapterIntent() {
+        bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
+            val bluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            activityResultLauncher.launch(bluetoothIntent)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun scanBluetoothLEDevices(enable: Boolean) {
+        when (enable) {
+            true -> {
+                handler.postDelayed({
+                    isScanning = false
+                    bluetoothAdapter?.bluetoothLeScanner?.stopScan(scanCallback)
+                }, 5000)
+            }
+            else -> {
+                isScanning = true
+                bluetoothAdapter?.bluetoothLeScanner?.startScan(scanCallback)
+            }
+        }
     }
 }
